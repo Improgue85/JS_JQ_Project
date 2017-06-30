f99063056b71662bbf8fde474b08e3e1ea196d5c
Improgue85 <brent.benjamin85@gmail.com>
2017-06-30 12:03:58 -0500

    edited css and created color function on click

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,33 @@
-$(document).ready(function createGrid() {
+function createGrid() {
 	for (var rows = 1; rows <= 16; rows++) {
 		for (var cols = 1; cols <= 16; cols++){
 			$("#box").append("<div class='grid'></div>")
 		};
 	};
-	$(".grid").height(50);
-	$(".grid").width(50);
+	$(".grid").height(43);
+	$(".grid").width(43);
+};
+
+function clearDrawing() {
+	$('#erase').remove();
+};
+
+$(document).ready(function(){
+	createGrid();
+
+	/*var square = false;
+	function draw() {
+		$(".grid").on('click', function(){
+			if (square) {
+				$(this).css('background-color', 'black');
+				square = false;
+			} else {
+				square = true;
+			};
+		});
+	};*/
+
+	$(".grid").on('click', function start() {
+		$(this).addClass("color").css('background-color', 'black');
+	});
 });
